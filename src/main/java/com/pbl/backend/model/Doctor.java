f2f9dd920696deee858ebee54d3f62d746cc0316
diemly26 <letrandiemly26@gmail.com>
2025-09-19 23:41:36 +0700

    fix bug unable to save name and phonenumber

--- src/main/java/com/pbl/backend/model/Doctor.java
+++ src/main/java/com/pbl/backend/model/Doctor.java
@@ -1,4 +1,25 @@
-package com.pbl.backend.model;
-
-public class Doctor {
-}
+//package com.pbl.backend.model;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Entity
+//@Table(name = "doctors")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Doctor extends User {
+//    private String gender;
+//    private Integer yoe;
+//
+//    @Column(length = 1000)
+//    private String introduction;
+//
+//    private String avatarFilepath;
+//}
